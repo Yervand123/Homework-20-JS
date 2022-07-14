faf663bab2e1a1ae25e2b28355803a8ea335fbc6
YERVAND <107866399+Yervand123@users.noreply.github.com>
2022-07-14 12:58:01 +0400

    Add files via upload

---
+++ homework 20 JS/script.js
@@ -0,0 +1,12 @@
+// let div = document.getElementById("div1");
+// let button = document.getElementById("buttonsubmit");
+
+
+// button.addEventListener("mousedown", function(evt){
+//     div.style.background = 'red';
+    
+//     })
+
+
+
+
